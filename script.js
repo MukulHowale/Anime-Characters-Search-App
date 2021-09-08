0e671c24d82b22f554e4c14584572e85b83d4df3
Mukul <howalemukul@gmail.com>
2021-09-09 00:15:24 +0530

    structure complete

--- script.js
+++ script.js
@@ -5,18 +5,75 @@ let timerId;
 
 
 let appendMovies = (m) =>{
-  // show.innerHTML = null;
+  show.innerHTML = null;
 
   console.log(m);
 
-  m.forEach(({name}) =>{
+  m.forEach(({name,anime,manga}) =>{
       let div = document.createElement('div');
 
-      div.innerText = name;
+      let h3 = document.createElement('h3');
+      h3.innerText = name;
+
+      let p2 = document.createElement('p');
+      p2.style.float = "left";
+
+      let p1 = document.createElement('p');
+      p1.style.float = "right";
+
+      if(anime.length != 0){
+        p1.textContent = "Anime";
+        if(anime[0].name.length >= 25){
+          p2.setAttribute('class','dots');
+        }
+        p2.textContent = anime[0].name;
+      }
+      else{
+        p1.textContent = "Manga";
+        if(manga[0].name.length >= 25){
+          p2.setAttribute('class','dots');
+        }
+        p2.textContent = manga[0].name;
+      }
+
+      let value = document.getElementById('val');
+      value.style.borderBottomLeftRadius = "0px";
+      value.style.borderBottomRightRadius = "0px";
+
+      div.append(h3,p2,p1);
 
       show.append(div);
       
   })
+
+  if(m.length >= 5){
+    show.style.overflow = "scroll";
+    show.style.overflowX = "hidden";
+    show.style.border = "1px solid black";
+    show.style.borderBottomRightRadius = "5px";
+  }
+  else if(m.length == 4){
+    show.style.border = "1px solid black";
+    show.style.borderBottomRightRadius = "20px";
+    if(show.style.overflowY){
+      show.style.overflowY = "hidden";
+    }
+  }
+  else{
+    let temp = show.firstChild;
+    while(temp != show.lastChild){
+      temp.style.borderLeft = "1px solid black";
+      temp.style.borderRight = "1px solid black";
+      temp = temp.nextSibling;
+    }
+    show.style.overflow = "hidden";
+    show.lastChild.style.borderLeft = "1px solid black";
+    show.lastChild.style.borderRight = "1px solid black";
+    show.lastChild.style.borderBottom = "1px solid black";
+    show.lastChild.style.borderBottomLeftRadius = "20px";
+    show.lastChild.style.borderBottomRightRadius = "20px";
+    show.style.border = "0px";
+  }
 }
 
 // get input value and calls append function
@@ -35,7 +92,7 @@ let main = async () =>{
 
   appendMovies(movies);
 
-  console.log(movies);
+  // console.log(movies);
 }
 
 //get data from server
@@ -43,6 +100,8 @@ let getData = async (n) =>{
   let res = await fetch(`https://api.jikan.moe/v3/search/character?q=${n}`);
 
   let data = await res.json();
+
+  return data.results;
 
   // console.log(data.results);
 }
